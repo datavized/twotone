aa5a408bac8733f124e61bd43e3de2184b5e4913
Brian Chirls <brian@technologyhumansandtaste.com>
2021-02-01 14:21:26 -0500

    Updates

---
+++ precache-manifest.1f0cca548ba5cc9e59162f8ac7475f9b.js
@@ -0,0 +1,324 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b51cd70c17a21e5ecead909a32dcb1",
+    "url": "673b6f265626e5264686.worker.js"
+  },
+  {
+    "revision": "96ec7cce2c2ad4e7fb7da4c917d2a6a4",
+    "url": "78d2219a8794b439d11a.worker.js"
+  },
+  {
+    "revision": "1190f1ecde8136f0a5efd996d3a5e5ed",
+    "url": "881e18de7b88e00dcff1.worker.js"
+  },
+  {
+    "revision": "9a63be053ab9965c262789caa73e628d",
+    "url": "a31df8ff4c832b796fa7.worker.js"
+  },
+  {
+    "revision": "0cdcda092ae5fadc11d2075fe0ceb888",
+    "url": "assets/android-chrome-144x144.png"
+  },
+  {
+    "revision": "8e65e0fb7aafd2f5fc26f42b2737bee8",
+    "url": "assets/android-chrome-192x192.png"
+  },
+  {
+    "revision": "679fa0986db9218a6a506fb3a9daf20e",
+    "url": "assets/android-chrome-256x256.png"
+  },
+  {
+    "revision": "163eafeb93db7eb64e613a56cf1d227f",
+    "url": "assets/android-chrome-36x36.png"
+  },
+  {
+    "revision": "e1cfff5c7e53c05c74f85787ec3b02ad",
+    "url": "assets/android-chrome-384x384.png"
+  },
+  {
+    "revision": "f049ed1cb5e3c8e2a148d8b307a35b6e",
+    "url": "assets/android-chrome-48x48.png"
+  },
+  {
+    "revision": "538cb76e90d014bfbbc2af4b748d0fca",
+    "url": "assets/android-chrome-512x512.png"
+  },
+  {
+    "revision": "63d88a525c720bee1dc926f6cf9cfb53",
+    "url": "assets/android-chrome-72x72.png"
+  },
+  {
+    "revision": "21a5f0bf517dc7e162c38c2850f0caa5",
+    "url": "assets/android-chrome-96x96.png"
+  },
+  {
+    "revision": "87d2511679bdd17376dbd4bd9713e902",
+    "url": "assets/apple-touch-icon-1024x1024.png"
+  },
+  {
+    "revision": "f1788caad3d64e854b5978136ae99d5e",
+    "url": "assets/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "f8bee68a52989956b10a4999d7f34fbb",
+    "url": "assets/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "d9d8cbbdc61ce505592225e35be9b327",
+    "url": "assets/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "51fb4b078e6aac0d05e8814bd0d2875b",
+    "url": "assets/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "7f47ad910fa66cbe9a7c329c5d33584d",
+    "url": "assets/apple-touch-icon-167x167.png"
+  },
+  {
+    "revision": "bc104d49afa784c81f38f1859a179c04",
+    "url": "assets/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "01a37bb3c79456bc4ef6c8c7074e756d",
+    "url": "assets/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "4f3ac9c2892c9aecef59f437df7288a1",
+    "url": "assets/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "9e320b322b1ec3e9ba207c233a4f7462",
+    "url": "assets/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "ef677874a9a2cf0c76792645befa6869",
+    "url": "assets/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "bc104d49afa784c81f38f1859a179c04",
+    "url": "assets/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "bc104d49afa784c81f38f1859a179c04",
+    "url": "assets/apple-touch-icon.png"
+  },
+  {
+    "revision": "3f52cd9e098cb7944f33a390188eb1fb",
+    "url": "assets/apple-touch-startup-image-1125x2436.png"
+  },
+  {
+    "revision": "eb4c8ee4a5ab03c64c0e87c5982ea887",
+    "url": "assets/apple-touch-startup-image-1136x640.png"
+  },
+  {
+    "revision": "8231b9b1387f8f2cab95fcde383d2801",
+    "url": "assets/apple-touch-startup-image-1242x2208.png"
+  },
+  {
+    "revision": "52ca4417b91db599356e87c84bbc762f",
+    "url": "assets/apple-touch-startup-image-1242x2688.png"
+  },
+  {
+    "revision": "c49412c951e9aa376d6ebc4806b87a2f",
+    "url": "assets/apple-touch-startup-image-1334x750.png"
+  },
+  {
+    "revision": "a0239a783bca5ca2b97100eb535d24f7",
+    "url": "assets/apple-touch-startup-image-1536x2048.png"
+  },
+  {
+    "revision": "0e8221e25289d859e175dbcd2d2912d9",
+    "url": "assets/apple-touch-startup-image-1620x2160.png"
+  },
+  {
+    "revision": "8150f10aee730b24e2c45a3c0177939d",
+    "url": "assets/apple-touch-startup-image-1668x2224.png"
+  },
+  {
+    "revision": "ba224faf1941362dff74f6241e819cf4",
+    "url": "assets/apple-touch-startup-image-1668x2388.png"
+  },
+  {
+    "revision": "6d818ff33effb8ad4085de5188aa2b5a",
+    "url": "assets/apple-touch-startup-image-1792x828.png"
+  },
+  {
+    "revision": "19f0086d8df04cd1760aca799f164ed0",
+    "url": "assets/apple-touch-startup-image-2048x1536.png"
+  },
+  {
+    "revision": "f0cdeccaa11ed357c62c1cb0949b139d",
+    "url": "assets/apple-touch-startup-image-2048x2732.png"
+  },
+  {
+    "revision": "5c125f9c608602b6cc74f8bcf728435e",
+    "url": "assets/apple-touch-startup-image-2160x1620.png"
+  },
+  {
+    "revision": "20e3cf3d32083d304a9bc140bf989673",
+    "url": "assets/apple-touch-startup-image-2208x1242.png"
+  },
+  {
+    "revision": "e4f41e8ab2ef96b1e69349b822b68db1",
+    "url": "assets/apple-touch-startup-image-2224x1668.png"
+  },
+  {
+    "revision": "e656b9ed9ad1a182bd40bb42db46cb61",
+    "url": "assets/apple-touch-startup-image-2388x1668.png"
+  },
+  {
+    "revision": "2dd551a70e99b9747fa88438cf0b3d7b",
+    "url": "assets/apple-touch-startup-image-2436x1125.png"
+  },
+  {
+    "revision": "fb514ff48d3c36726de3983325e4c4f9",
+    "url": "assets/apple-touch-startup-image-2688x1242.png"
+  },
+  {
+    "revision": "deb883e15a97e61a705e93d114bfd9af",
+    "url": "assets/apple-touch-startup-image-2732x2048.png"
+  },
+  {
+    "revision": "5816abb8b5927b025f73667ecda66ba7",
+    "url": "assets/apple-touch-startup-image-640x1136.png"
+  },
+  {
+    "revision": "adaa5f4f06baf50342b1988fbe08bc53",
+    "url": "assets/apple-touch-startup-image-750x1334.png"
+  },
+  {
+    "revision": "c97689f12d5d06ab78ae78539f889e1c",
+    "url": "assets/apple-touch-startup-image-828x1792.png"
+  },
+  {
+    "revision": "1a0cb11489931ba8794020c6ae510b48",
+    "url": "assets/browserconfig.xml"
+  },
+  {
+    "revision": "23dd11b17def0c008443824ca6dccc4b",
+    "url": "assets/coast-228x228.png"
+  },
+  {
+    "revision": "5606ed2f44fd464c64f7535cd3d74529",
+    "url": "assets/favicon-16x16.png"
+  },
+  {
+    "revision": "01c4435ce9db50d98b53d0c186105020",
+    "url": "assets/favicon-32x32.png"
+  },
+  {
+    "revision": "f049ed1cb5e3c8e2a148d8b307a35b6e",
+    "url": "assets/favicon-48x48.png"
+  },
+  {
+    "revision": "ea53fdcc744f75fdc9a4319c2aaa12bd",
+    "url": "assets/favicon.ico"
+  },
+  {
+    "revision": "2a387cd0e10c94d7b78b21b0a2f5a44b",
+    "url": "assets/firefox_app_128x128.png"
+  },
+  {
+    "revision": "fab23922eb81cfc4daa2df18135fb4bb",
+    "url": "assets/firefox_app_512x512.png"
+  },
+  {
+    "revision": "7e82c7ff35fae5252449f457f73fe51f",
+    "url": "assets/firefox_app_60x60.png"
+  },
+  {
+    "revision": "7ad052051e7b092615896b01acd08190",
+    "url": "assets/manifest.json"
+  },
+  {
+    "revision": "393dcc6d039b516ffe498a62e9a01a6f",
+    "url": "assets/manifest.webapp"
+  },
+  {
+    "revision": "0cdcda092ae5fadc11d2075fe0ceb888",
+    "url": "assets/mstile-144x144.png"
+  },
+  {
+    "revision": "1ce17159881e6ff9fe95abf43fac5652",
+    "url": "assets/mstile-150x150.png"
+  },
+  {
+    "revision": "280d58e2fc23e48863421087695d0e4a",
+    "url": "assets/mstile-310x150.png"
+  },
+  {
+    "revision": "2f66c9f39f791367f5203170bf9fd993",
+    "url": "assets/mstile-310x310.png"
+  },
+  {
+    "revision": "3d3af8fa146029ae7e639a6b8568a6f3",
+    "url": "assets/mstile-70x70.png"
+  },
+  {
+    "revision": "89d982f49ab75bc3879cfa704726b695",
+    "url": "assets/yandex-browser-50x50.png"
+  },
+  {
+    "revision": "ec8f172c7cec11c4a586a3173d4686dd",
+    "url": "assets/yandex-browser-manifest.json"
+  },
+  {
+    "url": "index-20266ed083f2f4c68731.js"
+  },
+  {
+    "url": "index-20f36d8c3edebddd88fc.js"
+  },
+  {
+    "url": "index-23d1a24a2ad421cd8d64.js"
+  },
+  {
+    "url": "index-310f0c6722cf7b607d03.js"
+  },
+  {
+    "url": "index-39b9019953ec791528d9.js"
+  },
+  {
+    "url": "index-47270d326041474945e8.js"
+  },
+  {
+    "url": "index-5420a90ebde75d19ae53.js"
+  },
+  {
+    "url": "index-6b3670a821bfda26f480.js"
+  },
+  {
+    "url": "index-79e543d16c5299f36c93.js"
+  },
+  {
+    "url": "index-89ab60f07c0e65427f90.js"
+  },
+  {
+    "url": "index-944c3ab3f1dd28fb43e7.js"
+  },
+  {
+    "url": "index-cf1ab95e4397cad8c818.js"
+  },
+  {
+    "url": "index-ef7e0d3237d063a8020a.js"
+  },
+  {
+    "url": "index-f77f4669a26ae08470e2.js"
+  },
+  {
+    "revision": "7d088e7add067db70379331ea77b3653",
+    "url": "index.html"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "f81670738878c4f2473fc5830d1de874",
+    "url": "static/media/78_i-dream-of-jeanie-with-the-light-brown-hair_glen-gray-and-the-casa-loma-orchestra-k_gbia0050476a_itemimage.f8167073.jpg"
+  },
+  {
+    "revision": "352f6f150bf2525385f337df485ac3fc",
+    "url": "static/media/Kimiko_Ishizaka_-_The_Open_Goldberg_Variations_-_20120529180117231.970f8cc2.jpg"
+  }
+]);
